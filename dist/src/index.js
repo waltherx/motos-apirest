8f1ce0cdbfabc2a01d54bb66ccf37d9e96425271
waltherx <waltermj2012@gmail.com>
2023-07-11 04:18:11 -0400

    swagger add

--- dist/src/index.js
+++ dist/src/index.js
@@ -7,6 +7,8 @@ var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var path_1 = __importDefault(require("path"));
 var dotenv_1 = __importDefault(require("dotenv"));
+var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+var swagger_json_1 = __importDefault(require("../docs/swagger.json"));
 var routes_1 = __importDefault(require("./routes/routes"));
 dotenv_1.default.config();
 var app = (0, express_1.default)();
@@ -19,6 +21,7 @@ app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'public'))
 app.get('/', function (req, res) {
     res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
 });
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.listen(port, function () {
     console.log("\u26A1\uFE0F[server]: Esta corriendo en -> \uD83E\uDD20 http://localhost:".concat(port, " \u26A1\uFE0F"));
 });
