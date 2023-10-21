4ed689f35c2a734b82bc8fc50c507cb674a4aae6
waltherx <waltermj2012@gmail.com>
2023-10-21 00:31:37 -0400

    update gitignore

--- dist/utils/token.utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*import { User } from '../models/userModel';
-
-const generateToken = (user: Partial<User>): string =>
-  jwt.sign(user, process.env.JWT_SECRET || 'superSecret', { expiresIn: '60d' });
-
-export default generateToken;
-*/ 
--- dist/utils/validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isIdValid = void 0;
-function isIdValid(id) {
-    return !isNaN(id) && id > 0;
-}
-exports.isIdValid = isIdValid;
